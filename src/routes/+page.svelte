234b69cf3c87388221c88720efa569360be7f0c2
Chika Ugorji <chikaugorji@outlook.com>
2026-04-16 13:11:12 -0700

    Experimented wit Framer motion, basic animations
    i used sv motion an api and i got it to do some basic things going and some animations now im ready to make the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,82 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import gsap from 'gsap';
-    import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-
-
-    let button;
-    // turns on scroll feat 
+    import { motion, useTime, useSpring, useTransform, useScroll } from '@humanspeak/svelte-motion'
     
-    gsap.registerPlugin(ScrollTrigger)
+    let targetEl: HTMLElement;
 
-    onMount(() => {
-        // This means the code only runs when the site is rendered for the user
-        console.log("The browser is ready and devleivered")
-        
-        gsap.fromTo(
-            ".button-hero", 
-            {x: 50, opacity: 0}, 
-        { x: 0, opacity: 1,duration: 1.5, ease: "bounce.out" })
-
-        button.addEventListener("mouseenter", () => {
-        gsap.to(button, {scale: 1.1, boxShadow: "0 25px 50px -12px rgba(291, 39, 119, 0.5)", duration: 0.3 });
-        
-        });
-        button.addEventListener("mouseleave", () => {
-        gsap.to(button, { scale: 1, boxShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.05)", duration: 0.3 });
+    const time = useTime()
+    const progress = useTransform(() => ($time % 4000) / 4000, [time])
+    
+    const { scrollYProgress } = useScroll()
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
     });
-        
-    });
+    // as scrolll goes from 0 to 1 opacity goes from 
+    const opacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
+    // as scrolll goes from 0 to 1 text moves from 0 to -200 xp
+    const y = useTransform(scrollYProgress, [0, 1], [0, -500]);
+    const cat_opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
+    // as scrolll goes from 0 to 1 text moves from 0 to -200 xp
+    const cat_y = useTransform(scrollYProgress, [0.3, 1], [0, -200]);
 
+    
 </script>
 
+<div class="fixed top-0 left-0 right-0 h-2 bg-blue-500 origin-left z-50"
+  style:transform="scaleX({$scaleX})">
 
-<main class="lottie-anime min-h-screen flex flex-col items-center justify-center bg-black">
-    <section class=" min-w-full min-h-auto bg-emerald-600 text-white p-6">
+
+</div>
+<main class="lottie-anime flex flex-col items-center justify-center bg-black">
+    <section class=" min-w-full min-h-screen bg-emerald-600 text-white p-6">
         <h1 class="text-7xl"> Welcome to Weather dashboard</h1>
-    </section>
+        <motion.ul drag dragSnapToOrigin
+        class="size-24 rounded-md bg-pink-400"
+        animate={{ rotate: 360 }}
+        transition={{duration: 1}}
 
-    <section class="site-hero min-w-full min-h-auto bg-amber-900 text-white p-6 flex flex-col">
-        <div class="text-center">
-            <button bind:this={button} class="button-hero text-2xl bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xs shadow-pink-600 font-semibold rounded-3xl p-[.5em] m-[2em] cursor-pointer ease-in-out hover:scale-110 hover:shadow-2xl">
-                Click me
-            </button>
+        />
+        <div class="mt-[clamp(9rem,12vw,15rem)] w-full h-full flex flex-col items-center">
+            <h1 class="text-7xl"
+            style:opacity={$opacity} style:transform="translateY({$y}px)"
+            > 
+                Hello World
+            </h1>
+        </div>
+        <div>
+        <img alt="pfp" src="tab_cat_icon.jpg" class="size-24 rounded-2xl"
+        style:opacity={$cat_opacity} style:transform="translateY({$cat_y}px)">
         </div>
     </section>
 
-    <section class="min-w-full min-h-auto bg-cyan-700 text-white p-6">
+    <!-- <section class=" h-[200vh] bg-slate-900 test-white p-20">
+        
+
+    </section>
+    -->
+
+    <section class="site-hero min-w-full min-h-screen bg-amber-900 text-white p-6 flex flex-col">
+        <div class="text-center">
+            <motion.button
+            class="button-hero text-2xl bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xs shadow-pink-600 font-semibold rounded-3xl p-[.5em] m-[2em] cursor-pointer hover:shadow-2xl"
+            initial={{ scale: 0, opacity: 0, y:100 }}
+            whileInView={{ 
+                scale: 1,
+                opacity: 1,
+                 y: 0,
+                transition: 2 
+                }}
+            whileHover={{ scale: 1.2}}
+            whileTap={{ scale: 0.8 }}
+            >
+            Click me
+            </motion.button>
+        </div>
+        
+    </section>
+
+    <section class="min-w-full min-h-screen bg-cyan-700 text-white p-6">
     About
     </section>
 </main>
