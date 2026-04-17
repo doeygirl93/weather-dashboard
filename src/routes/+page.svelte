8b755f6b4003346203aaa2e23b5d5f3569aab1e1
Chika Ugorji <chikaugorji@outlook.com>
2026-04-16 19:50:23 -0700

    Finished Hero
    polished it, made it blend into the background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { motion, useTime, useSpring, useTransform, useScroll } from '@humanspeak/svelte-motion'
-
     const time = useTime()
     const progress = useTransform(() => ($time % 4000) / 4000, [time])
     
@@ -15,7 +14,7 @@
     // as scrolll goes from 0 to 1 text moves from 0 to -200 xp
     const y1 = useTransform(scrollYProgress, [0, 1], [0, -100]);
     const y2 = useTransform(scrollYProgress, [0, 1], [0, -125]);
-    const y3 = useTransform(scrollYProgress, [0, 1], [0, 600]);
+    const y3 = useTransform(scrollYProgress, [0, 1], [0, 20]);
 
 </script>
 
@@ -26,10 +25,12 @@
   style:transform="scaleX({$scaleX})">
 </div>
 
+
+
 <main class="flex flex-col items-center justify-center bg-black">
-    <section class=" min-w-full min-h-[90dvh] bg-[url('spacex-VBNb52J8Trk-unsplash.jpg')] bg-cover bg-center"
-    style:transform="translateY({$y3}px)"
-    >
+    <section class="min-w-full min-h-[90dvh] bg-[linear-gradient(to_bottom,rgba(0,0,0,0.05)_0%,rgba(0,0,0,0)_80%,#031523_100%),url('spacex-VBNb52J8Trk-unsplash.jpg')] bg-cover bg-center"
+    style:transform="translateY({$y3}px)">
+
 
         <div class="mt-[clamp(5rem,10vw,18rem)] w-full h-full flex flex-col items-center text-center">
             <h1 class="text-7xl font-semibold tracking-tighter bg-linear-to-b from-blue-800 to-blue-950 bg-clip-text text-transparent"
@@ -45,26 +46,26 @@
         >
         Made wit 🥶 by Queen Von
         </p>
-        
-        
+
 
         <!-- Stickers-->
          <div class="flex justify-end w-full">
         <motion.ul drag
-        class="size-24 rounded-md bg-[url('rocket.png')] bg-cover bg-center"
+        class="size-42 rounded-md bg-[url('rocket.png')] bg-cover bg-center"
         animate={{ rotate: 360 }}
         transition={{duration: 1}} />
         </div>
         <div class="flex justify-start w-full">
             <motion.ul drag
-            class="size-24 rounded-md bg-[url('handsome_orphues.png')] bg-cover bg-center flex justify-end"
+            class="size-42 rounded-md bg-[url('handsome_orphues.png')] bg-cover bg-center flex justify-end"
             animate={{ rotate: 360 }}
             transition={{duration: 1}} />
         </div>
         <div class="flex flex-col items-center w-full">
             <motion.ul drag
             class="size-42 rounded-md bg-[url('planet.png')] bg-cover bg-center flex justify-end"
-            animate={{ opacity: 0 }}
+            initial={{ scale: 0, opacity: 0, x:101 }}
+            animate={{  scale: 1, opacity: 1, x: 0  }}
             transition={{duration: 1}} />
         </div>
     </section>
@@ -77,8 +78,24 @@
 
 
     <section class="site-hero min-w-full min-h-[190dvh] bg-[#031523] text-white p-6 flex flex-col">
-        <div class="text-center">
         
+        
+        <!-- Button -->
+        <div class="text-center">
+         <motion.button
+            class="button-hero text-2xl bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xs shadow-pink-600 font-semibold rounded-3xl p-[.5em] m-[2em] cursor-pointer hover:shadow-2xl"
+            initial={{ scale: 0, opacity: 0, y:100 }}
+            whileInView={{ 
+                scale: 1,
+                opacity: 1,
+                 y: 0,
+                transition: 2 
+                }}
+            whileHover={{ scale: 1.2}}
+            whileTap={{ scale: 0.8 }}
+            >
+            Click me
+            </motion.button>
         </div>
         
     </section>
