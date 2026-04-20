7eb4e9e15646a39283f7e4dcb671b8d117686acc
Chika Ugorji <chikaugorji@outlook.com>
2026-04-20 01:51:03 -0700

    Finished most of dashboard
    i just got to fix the zen quotes also i got the API's working and stuff

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { PUBLIC_NASA_API_KEY } from '$env/static/public';
     import { motion} from '@humanspeak/svelte-motion'
 
     import { onMount } from 'svelte';
@@ -11,6 +12,66 @@
 
     let timeString = $derived(time.toLocaleTimeString());
 
+    let backgroundImage = $state("")
+
+    let lat = $state(0);
+    let lon = $state(0);
+    let weather = $state({ temp: "--", description: "Loading Data . . . "});
+
+    let quote = $state({text: "Loading sora AI + Chatgpt . ..  . . . ", author: "Ur gurl"});
+
+    async function getNasaPhoto() {
+        try {
+            const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${PUBLIC_NASA_API_KEY}`);
+
+            if (!response.ok) throw new Error("NASA API connection failed");
+
+            const data = await response.json();
+
+            backgroundImage = data.url;
+        } catch (error) {
+            console.error("Cosmic Error:", error);
+            backgroundImage = "bc_bg_img.jpg"
+        }
+    }
+
+    async function getWeather() {
+        // gets cords from browser
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+
+            try {
+                // get weather basrd on the corsd
+                
+                const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code&temperature_unit=fahrenheit`)
+                const data = await res.json();
+
+                weather = {
+                    temp: Math.round(data.current.temperature_2m),
+                    description: "Conditions: "
+                };
+            } catch (e) {
+                console.error("Weather API failed", e);
+           }
+        }); 
+    }
+
+        async function getQuotes() {
+            try{
+            const res = await fetch('https://corsproxy.io/?https://zenquotes.io/api/random')
+            const data = await res.json();
+
+            quote = {
+                text: data[0].q,
+                author: data[0].a
+            };
+
+        } catch (e) {
+            quote = { text: "You must be Git, because I can’t commit without you ", author: "r/ProgrammerHumor" };
+        }
+    };
+
 
     // runs when the user first wants acess
     function auth() {
@@ -20,7 +81,7 @@
             // this stsate is what comfirms when we swithc the ui
             isAuth = true;
         }
-    }
+    };
 
     // supposed to get the user's name if they have saved it
     onMount (() => {
@@ -34,11 +95,15 @@
             time= new Date()
         }, 1000);
 
+        getNasaPhoto();
+
+        getWeather();
+
+        getQuotes();
+
         return () => clearInterval(interval);
 
-        
-
-    });
+        });
 </script>
 
 <svelte:head>
@@ -98,14 +163,58 @@
         
     </main>
 {:else}
-    <section>
-        <div class="clock-box">
-            <p class="text-6xl font-mono ">{timeString}</p>
-        </div>
-    
-        <div in:fade={{ delay: 400}} class="">
+    <main
+    class="h-screen w-full bg-cover bg-cover transition-all duration-1000 flex items-center justify-center relative overflow-hidden"
+    style="background-image:url('{backgroundImage}')"
+    >
+
+        <div class="absolute inset-0 bg-black/40 backdrop-blur-[2px]"></div>
+
+        <section class="relative z-10 text-center">
+            <div class="clock-box bg-white/10 backdrop-blur-xl p-10 rounded-3xl border border-white/20">
+                <h2 class="text-green-400 font-mono tracking-widest test-sm mb-2 uppercase">Welcome Back {userName}</h2>
+                <p class="text-8xl font-mono text-white font-bold drop-shadow-2xl">{timeString}</p>
+                <div class="flex justify-around items-center font-mono">
+                    <div class="text-left">
+                        <p> External Temo </p>
+                        <p> {weather.temp} C</p>
+                    </div>
+                    <!--
+                    <div class="text-right">
+                        <p> Weather Desc </p>
+                        <p> {weather.description} C</p>
+                    </div>
+                    -->
+
+                </div>
+
+            </div>
+
+        
+            <div in:fade={{ delay: 400}} class="">
+            <p class="text-shadow-[-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000,1px_1px_0_#000] text-white italic"> Welcome back {userName}</p>
 
 
-        </div>
-    </section>
+            </div>
+
+            <footer class="absolute bottom-[-60] left-0 w-full flex flex-col items-center px-4">
+                <div class="w-2xl text-center cursor-pointer" >
+                    <p class="text-white/80 font-mono italic text-lg drop-shadow-md transition-all hover:text-white">
+                        "{quote.text}"
+                    </p>
+                    <p class="text-white/80 font-mono italic text-lg drop-shadow-md transition-all hover:text-white">
+                        "{quote.author}"
+                    </p>
+
+                    <button onclick={getQuotes}>
+                        <span class="text-green-400/80 font-mono italic text-lg drop-shadow-md transition-all hover:scale-110 hover:text-green-400">
+                            Click For Further enlightenment
+                        </span>
+                    </button>
+                </div>
+            
+            </footer>
+        </section>
+
+    </main>
 {/if}
